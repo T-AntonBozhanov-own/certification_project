4d6835113b13292f146eefb6f41b9e60e7330fa9
AntonBozhanov <anton.bozhanov@yalantis.com>
2024-03-12 22:45:12 -0700

    add db connection

---
+++ server/config/mongodb.js
@@ -0,0 +1,28 @@
+const path = require('path')
+require('dotenv').config({path: path.join(__dirname, '../.env')})
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const makeConnection = async () => {
+    const dbUrl = process.env.MONGO_URL
+    
+    try {
+        console.log(`Connecting to .... ${dbUrl}`)
+        await mongoose.connect(dbUrl)
+        console.log('Successfully connected to DB')
+    } catch(e) {
+        console.log('Error while connecting to DB', e)
+    }
+}
+
+const closeConnection = async () => {
+    try {
+        await mongoose.connection.close()
+    } catch (e) {
+        console.log('Error while closing connection', e)
+    }
+}
+makeConnection()
+
+module.exports = { makeConnection, closeConnection }
